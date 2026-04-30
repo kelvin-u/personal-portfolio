2c26cfe1bd4a7493e605ac21cb81d7f5764f465b
Kelvin Yu <yukaiwenn@gmail.com>
2026-04-30 16:11:47 -0400

    updated project section

--- js/custom.js
+++ js/custom.js
@@ -420,32 +420,4 @@ $(document).ready(function () {
     once: true,
     disable: "mobile",
   });
-
-  //  isotope
-  $("#projects").waitForImages(function () {
-    var $container = $(".portfolio_container");
-    $container.isotope({
-      filter: "*",
-    });
-
-    $(".portfolio_filter a").click(function () {
-      $(".portfolio_filter .active").removeClass("active");
-      $(this).addClass("active");
-
-      var selector = $(this).attr("data-filter");
-      $container.isotope({
-        filter: selector,
-        animationOptions: {
-          duration: 500,
-          animationEngine: "jquery",
-        },
-      });
-      return false;
-    });
-  });
-
-  //animatedModal
-  $(
-    "#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09",
-  ).animatedModal();
 });
